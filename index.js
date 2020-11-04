3016be417326755be12d9ff2b1db4f57a165f51b
will Farrell <will.farrell@gmail.com>
2020-11-04 13:48:31 -0700

    fix: update metadata logic

--- index.js
+++ index.js
@@ -5,51 +5,16 @@ const { snakeCase } = require('change-case')
 const encode = require('./lib/encode')
 const coordinate = require('./lib/coordinate')
 const {characteristic, distance} = require('./lib/measure')
-const methodSpeciation = require('./lib/methodSpeciation')
+//const methodSpeciation = require('./lib/methodSpeciation')
 const sanitize = require('./lib/sanitize')
 const time = require('./lib/time')
-
-let metadata = {
-  observations: 0,
-  spatial_extent: [-180, -90, 180, 90],
-  vertical_extent: [-Infinity, Infinity],
-  temporal_extent: [-Infinity, Infinity]
-}
-
-const getMetadata = () => {
-
-  metadata.temporal_extent = metadata.temporal_extent.map(date => {
-    if (date && Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date)) {
-      return date.toISOString()
-    }
-    return date  // Infinity
-  })
-
-  // ensure extent arrays are in proper format
-  metadata.spatial_extent = `BOX(${metadata.spatial_extent[0]} ${metadata.spatial_extent[1]}, ${metadata.spatial_extent[2]} ${metadata.spatial_extent[3]})`
-  metadata.temporal_extent = `[${metadata.temporal_extent[0]},${metadata.temporal_extent[1]}]`
-    .replace('[-Infinity', '(')
-    .replace('Infinity]', ')')
-  metadata.vertical_extent = `[${metadata.vertical_extent[0]},${metadata.vertical_extent[1]}]`
-    .replace('[-Infinity', '(')
-    .replace('Infinity]', ')')
-
-  return metadata
-}
-
-const setMetadata = (values) => {
-  // requires { id, create_timestamp } are passed in
-  // remove nulls
-  Object.keys(values).forEach(k => {
-    if (!values[k]) delete values[k]
-  } )
-  metadata = Object.assign({}, metadata, values)
-}
+const metadata = require('./lib/metadata')
 
 const stream = new Transform({
   readableObjectMode: true,
   writableObjectMode: true,
   async transform (chunk, encoding, callback) {
+    chunk = JSON.parse(chunk.toString())
 
     // bad data in legacy dataset (72beacba-edac-4b71-beeb-d7d7eb371725 v1.0.0 has μS/cm)
     chunk.ResultUnit = encode(chunk.ResultUnit)
@@ -64,9 +29,12 @@ const stream = new Transform({
       record[snakeCase(key)] = chunk[key]
     })
 
-    makeMetadata(record)
+    metadata.update(record)
 
-    callback(null, record)
+    delete record.monitoring_location_latitude_normalized
+    delete record.monitoring_location_longitude_normalized
+
+    callback(null, JSON.stringify(record))
   }
 })
 
@@ -241,87 +209,11 @@ const makeObservation = (data) => {
     AnalysisStartTimeZone,
 
     // Internal
-    DatasetID: metadata.id,
-    CreateTimestamp: metadata.create_timestamp // for datastream growth metrics and rollback edge case (multi preview patches)
+    DatasetID: metadata.getId,
   }
 
   return record
 }
 
 
-const makeMetadata = (data) => {
-  // t [min,max]
-  if ((data.activity_start_date || null) !== null) {
-    const minTime = new Date(`${data.activity_start_date}T${data.activity_start_time || '00:00:00'}`)
-    const maxTime =
-      (data.activity_end_date || null) !== null
-        ? new Date(
-        Math.max(minTime, new Date(`${data.activity_end_date}T${data.activity_end_time || '00:00:00'}`))
-        )
-        : minTime
-    if (metadata.temporal_extent[0] === -Infinity) {
-      metadata.temporal_extent = [minTime, maxTime]
-    }
-
-    metadata.temporal_extent[0] = new Date(
-      Math.min(metadata.temporal_extent[0], minTime)
-    )
-    metadata.temporal_extent[1] = new Date(
-      Math.max(metadata.temporal_extent[1], maxTime)
-    )
-  }
-  // x,y [xmin,ymin,xmax,ymax]
-  if (
-    data.monitoring_location_longitude !== null &&
-    data.monitoring_location_latitude !== null
-  ) {
-    if (metadata.spatial_extent[0] === -180) {
-      metadata.spatial_extent = [
-        data.monitoring_location_longitude,
-        data.monitoring_location_latitude,
-        data.monitoring_location_longitude,
-        data.monitoring_location_latitude
-      ]
-    }
-    metadata.spatial_extent[0] = Math.min(
-      metadata.spatial_extent[0],
-      data.monitoring_location_longitude
-    )
-    metadata.spatial_extent[1] = Math.min(
-      metadata.spatial_extent[1],
-      data.monitoring_location_latitude
-    )
-    metadata.spatial_extent[2] = Math.max(
-      metadata.spatial_extent[2],
-      data.monitoring_location_longitude
-    )
-    metadata.spatial_extent[3] = Math.max(
-      metadata.spatial_extent[3],
-      data.monitoring_location_latitude
-    )
-  }
-
-  // z [min,max]
-  if (data.activity_depth_height_measure_normalized !== null) {
-    if (metadata.vertical_extent[0] === -Infinity) {
-      metadata.vertical_extent = [
-        data.activity_depth_height_measure_normalized,
-        data.activity_depth_height_measure_normalized
-      ]
-    }
-    metadata.vertical_extent[0] = Math.min(
-      metadata.vertical_extent[0],
-      data.activity_depth_height_measure_normalized
-    )
-    metadata.vertical_extent[1] = Math.max(
-      metadata.vertical_extent[1],
-      data.activity_depth_height_measure_normalized
-    )
-  }
-
-  metadata.observations += 1
-
-  return metadata
-}
-
-module.exports = {stream, getMetadata, setMetadata}
+module.exports = {stream}
---
+++ test/metadata.js
@@ -0,0 +1,253 @@
+/* global describe, it */
+const chai = require('chai')
+const expect = chai.expect
+
+const metadata = require('../lib/metadata')
+
+console.error = () => {}
+
+describe('Metadata', function () {
+  it('should get default values', () => {
+    metadata.set()
+    const value = metadata.get()
+
+    expect(value.observations).to.equal(0)
+  })
+
+  it('should get id', () => {
+    metadata.set({ id: '00000000-0000-0000-0000-000000000000' })
+    const value = metadata.get()
+
+    expect(value.id).to.equal('00000000-0000-0000-0000-000000000000')
+    expect(metadata.getId()).to.equal('00000000-0000-0000-0000-000000000000')
+  })
+
+  describe('Temporal', function () {
+    it('should process null', () => {
+      const min = '2010-01-01', max = '2020-02-02'
+      metadata.set({temporal_extent:null})
+      metadata.update({
+        activity_start_date: min
+      })
+      metadata.update({
+        activity_start_date: max
+      })
+      const value = metadata.get()
+
+      expect(value.temporal_extent).to.equal(`[${min}T00:00:00.000Z,${max}T00:00:00.000Z]`)
+    })
+
+    it('should process empty', () => {
+      const min = '2010-01-01', max = '2020-02-02'
+      metadata.set({temporal_extent:'(,)'})
+      metadata.update({
+        activity_start_date: min
+      })
+      metadata.update({
+        activity_start_date: max
+      })
+      const value = metadata.get()
+
+      expect(value.temporal_extent).to.equal(`[${min}T00:00:00.000Z,${max}T00:00:00.000Z]`)
+    })
+
+    it('should update existing', () => {
+      const min = '1980-01-01', max = '2020-02-02'
+      metadata.set({temporal_extent:'[2000-01-01T00:00:00.000Z,2000-01-01T00:00:00.000Z]'})
+      metadata.update({
+        activity_start_date: min
+      })
+      metadata.update({
+        activity_start_date: min,
+        activity_end_date: max
+      })
+      const value = metadata.get()
+
+      expect(value.temporal_extent).to.equal(`[${min}T00:00:00.000Z,${max}T00:00:00.000Z]`)
+    })
+
+    it('should not update existing', () => {
+      const min = '2010-01-01', max = '2020-02-02'
+      metadata.set({temporal_extent:'[1900-01-01T00:00:00.000Z,2100-01-01T00:00:00.000Z]'})
+      metadata.update({
+        activity_start_date: min
+      })
+      metadata.update({
+        activity_start_date: max
+      })
+      const value = metadata.get()
+
+      expect(value.temporal_extent).to.equal(`[1900-01-01T00:00:00.000Z,2100-01-01T00:00:00.000Z]`)
+    })
+  })
+
+  describe('Spatial', function () {
+    it('should process null', () => {
+      const min = -1, max = 1
+      metadata.set({spatial_extent:null})
+      metadata.update({
+        monitoring_location_longitude_normalized: min,
+        monitoring_location_latitude_normalized: min
+      })
+      metadata.update({
+        monitoring_location_longitude_normalized: max,
+        monitoring_location_latitude_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.spatial_extent).to.equal(`BOX(${min} ${min}, ${max} ${max})`)
+    })
+
+    it('should process on zero', () => {
+      const min = 0, max = 0
+      metadata.set({spatial_extent:'BOX(0 0, 0 0)'})
+      metadata.update({
+        monitoring_location_longitude_normalized: min,
+        monitoring_location_latitude_normalized: min
+      })
+      metadata.update({
+        monitoring_location_longitude_normalized: max,
+        monitoring_location_latitude_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.spatial_extent).to.equal(`BOX(${min} ${min}, ${max} ${max})`)
+    })
+
+    it('should process to zero', () => {
+      const min = 0, max = 10
+      metadata.set({spatial_extent:'BOX(10 10, 10 10)'})
+      metadata.update({
+        monitoring_location_longitude_normalized: min,
+        monitoring_location_latitude_normalized: min
+      })
+      metadata.update({
+        monitoring_location_longitude_normalized: max,
+        monitoring_location_latitude_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.spatial_extent).to.equal(`BOX(${min} ${min}, ${max} ${max})`)
+    })
+
+    it('should update existing', () => {
+      const min = -1, max = 1
+      metadata.set({spatial_extent:'BOX(0 0, 0 0)'})
+      metadata.update({
+        monitoring_location_longitude_normalized: min,
+        monitoring_location_latitude_normalized: min
+      })
+      metadata.update({
+        monitoring_location_longitude_normalized: max,
+        monitoring_location_latitude_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.spatial_extent).to.equal(`BOX(${min} ${min}, ${max} ${max})`)
+    })
+
+    it('should not update existing', () => {
+      const min = -1, max = 1
+      metadata.set({spatial_extent:'BOX(-100 -100, 100 100)'})
+      metadata.update({
+        monitoring_location_longitude_normalized: min,
+        monitoring_location_latitude_normalized: min
+      })
+      metadata.update({
+        monitoring_location_longitude_normalized: max,
+        monitoring_location_latitude_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.spatial_extent).to.equal(`BOX(-100 -100, 100 100)`)
+    })
+  })
+
+  describe('Vertical', function () {
+    it('should process null', () => {
+      const min = -1, max = 1
+      metadata.set({vertical_extent:null})
+      metadata.update({
+        activity_depth_height_measure_normalized: min
+      })
+      metadata.update({
+        activity_depth_height_measure_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.vertical_extent).to.equal(`[${min},${max}]`)
+    })
+
+    it('should process empty', () => {
+      const min = -1, max = 1
+      metadata.set({vertical_extent:'(,)'})
+      metadata.update({
+        activity_depth_height_measure_normalized: min
+      })
+      metadata.update({
+        activity_depth_height_measure_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.vertical_extent).to.equal(`[${min},${max}]`)
+    })
+
+    it('should process on zero', () => {
+      const min = 0, max = 0
+      metadata.set({vertical_extent:'[0,0]'})
+      metadata.update({
+        activity_depth_height_measure_normalized: min
+      })
+      metadata.update({
+        activity_depth_height_measure_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.vertical_extent).to.equal(`[${min},${max}]`)
+    })
+
+    it('should process to zero', () => {
+      const min = 0, max = 10
+      metadata.set({vertical_extent:'[10,10]'})
+      metadata.update({
+        activity_depth_height_measure_normalized: min
+      })
+      metadata.update({
+        activity_depth_height_measure_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.vertical_extent).to.equal(`[${min},${max}]`)
+    })
+
+    it('should update existing', () => {
+      const min = -1, max = 1
+      metadata.set({vertical_extent:'[0,0]'})
+      metadata.update({
+        activity_depth_height_measure_normalized: min
+      })
+      metadata.update({
+        activity_depth_height_measure_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.vertical_extent).to.equal(`[${min},${max}]`)
+    })
+
+    it('should not update existing', () => {
+      const min = -1, max = 1
+      metadata.set({vertical_extent:'[-100,100]'})
+      metadata.update({
+        activity_depth_height_measure_normalized: min
+      })
+      metadata.update({
+        activity_depth_height_measure_normalized: max
+      })
+      const value = metadata.get()
+
+      expect(value.vertical_extent).to.equal(`[-100,100]`)
+    })
+  })
+
+
+})
